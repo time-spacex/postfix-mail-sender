61d22690af2550a4401a5df6866db76e2777c04e
r.kleshnev <r.kleshnev@ddos-guard.net>
2024-09-06 12:47:48 +0300

    fix relay ip adress to internal

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selec
 echo "postfix postfix/mailname string test-vm-1" | debconf-set-selections
 apt install postfix -y
 postconf maillog_file=/var/log/postfix.log
-postconf relayhost=185.129.102.80
+postconf relayhost=10.2.22.98
